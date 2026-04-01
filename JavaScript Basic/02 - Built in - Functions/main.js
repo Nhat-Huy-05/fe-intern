95d5e170d262dfa475489d4c4a1fe7b494c41642
Riyu <bui85847@gmail.com>
2026-04-01 17:55:05 +0700

    Enhance comments for console methods in main.js
    Added comments explaining console methods and their usage.

--- JavaScript Basic/02 - Built in - Functions/main.js
+++ JavaScript Basic/02 - Built in - Functions/main.js
@@ -2,4 +2,12 @@
 var age = 20;
 alert(age);
 
-//2.Console:
+/*2.Console: Là 1 đối tượng(object),cung cấp quyền truy cập
+    vào bảng điều khiển gỡ lỗi của trình duyệt, 
+    môi trường chạy code(Node.js)
+*/
+console.log(age);
+console.error(age); //in ra tb lỗi
+console.warn(age);  //in ra tb cảnh báo
+
+//3.Confirm: Hiển thị 
