699d09b25478decd625b18faf0732bd20e7f162e
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-06 16:59:48 +0700

    Update

--- ViDu_JS/vidu.js
+++ ViDu_JS/vidu.js
@@ -1,6 +1,8 @@
-function writeLog(){
-  for (var param of arguments){
-    console.log(param);
-  }
+function tinhTong(a, b) {
+    return a + b; // Nhả kết quả ra ngoài
 }
-writeLog('hello 1','hello 2','hello 3');
+
+let ketQua = tinhTong(5, 10); 
+// ketQua bây giờ bằng 15.
+// Bạn có thể dùng nó để tính tiếp:
+console.log(ketQua * 2); // Kết quả: 30
