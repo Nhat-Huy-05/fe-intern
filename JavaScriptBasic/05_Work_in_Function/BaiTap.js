1819e5619e0a61428cbc599913710b7c01ec80d9
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-07 09:07:56 +0700

    Update

---
+++ JavaScriptBasic/05_Work_in_Function/BaiTap.js
@@ -0,0 +1,8 @@
+/*Viết hàm tinhHieu(a, b ) trừ 2 số và trả về KQ.
+  Sau đó gọi hàm với 2 số bất kỳ.
+*/
+function tinhHieu(a, b){
+    return a - b;
+}
+var result = tinhHieu(5, 4);
+console.log(result);
