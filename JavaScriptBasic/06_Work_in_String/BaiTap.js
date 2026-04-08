63f0015038e5a6bde31605ca6debf5469e981bea
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-08 14:45:06 +0700

    Fix: sửa lỗi

--- JavaScriptBasic/06_Work_in_String/BaiTap.js
+++ JavaScriptBasic/06_Work_in_String/BaiTap.js
@@ -1,9 +1,5 @@
-var fullName = 'Nhat Huy'; //1.
-var fullName = new String('Nhat Huy'); //2.
-alert(fullName);
-console.log(typeof fullName);
-/*Sự khác nhau giữa 2 cách: cách 2 dài dòng hơn, sẽ khác kiểu dl
- nó sẽ khác nhau giữa kiểu dl, cái 1 sẽ là string,
- còn cái 2 được hiểu là object 
- Recommend: xài cách 1
- */
+var user = "Học viên";
+var exercise = "JavaScript";
+
+var mess = "Chào " + user + ", bạn đang học " + exercise;
+console.log(mess);
