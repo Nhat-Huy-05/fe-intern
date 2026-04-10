17bf38bd507403533730add443f204e8b54bfce8
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-10 09:54:36 +0700

    Update

--- JavaScriptBasic/08_Array/main.js
+++ JavaScriptBasic/08_Array/main.js
@@ -28,3 +28,50 @@ var subFruits = moreFruits.slice(0, 2);
 // Kết quả: ['Orange', 'Grapes']
 
 console.log('Mảng cuối cùng:', moreFruits);
+
+//Bài tập 1:
+/*Cho trước hàm joinWithCharacter có 2 tham số là array và charactor, 
+hãy hoàn thành phần nội dung hàm sao cho hàm trả về 1 chuỗi là kết quả của việc nối từng phần tử của mảng với charactor. */
+function joinWithCharacter(array, charactor) {
+    return array.join(charactor)
+}
+
+
+// Ví dụ khi sử dụng
+var cars = ['Honda', 'Mazda', 'Mercedes'];
+
+var result = joinWithCharacter(cars, ' - ');
+
+console.log(result); // Expected: "Honda - Mazda - Mercedes"
+
+//Bài tập 2:
+/*Hãy tạo hàm getLastElement có 1 tham số (F8 sẽ gọi hàm này và luôn truyền đối số là 1 array), 
+  hàm này sẽ trả về phần tử cuối cùng trong mảng
+ */
+// Viết hàm tại đây
+function getLastElement(array){
+    return array[array.length - 1];
+}
+
+// Ví dụ sử dụng
+var animals = ['Monkey', 'Tiger', 'Elephant'];
+var result = getLastElement(animals);
+
+console.log(result); // Expected: "Elephant"
+console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
+
+//Bài tập 3:
+/*Hãy tạo hàm getFirstElement có 1 tham số (F8 sẽ gọi hàm này và luôn truyền đối số là 1 array), 
+hàm này sẽ trả về phần tử đầu tiên trong mảng. 
+*/
+// Viết hàm tại đây
+function getFirstElement(array){
+    return array[0];
+}
+
+// Ví dụ sử dụng
+var animals = ['Monkey', 'Tiger', 'Elephant'];
+var result = getFirstElement(animals);
+
+console.log(result); // Expected: "Monkey"
+console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
