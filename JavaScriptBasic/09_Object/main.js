7f066576921b47f63334471193de5d62e33925aa
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-10 11:25:51 +0700

    Update

--- JavaScriptBasic/09_Object/main.js
+++ JavaScriptBasic/09_Object/main.js
@@ -31,3 +31,22 @@ function Animal(name, leg, speed){
 var parrot = new Animal("parrot",2, 100);
 console.log(parrot);
 
+//Bài tập 2:
+/*Hãy tạo một object constructor Student gồm: firstName, lastName. Sau đó, định nghĩa thêm phương thức là getFullName,
+ phương thức này sẽ trả về tên đầy đủ của sinh viên.
+Giữa firstName và lastName cần có 1 khoảng trắng (1 dấu space).
+*/
+function Student(firstName,lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+Student.prototype.getFullName = function(){
+        return this.firstName +" "+this.lastName
+    };
+// Ví dụ khi sử dụng
+var student = new Student('Long', 'Bui');
+
+console.log(student.firstName);  // 'Long'
+console.log(student.lastName);  // 'Bui'
+console.log(student.getFullName());  // 'Long Bui'
+
