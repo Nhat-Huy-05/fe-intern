faa9a73bfc2476a63d1eb1c4d1035c6dc7644372
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-14 10:35:50 +0700

    Fix

--- ViDu_JS/vidu.js
+++ ViDu_JS/vidu.js
@@ -1,1 +1,30 @@
 
+
+var sports = [
+    {
+        name: 'Bơi lội',
+        gold: 11
+    },
+    {
+        name: 'Boxing',
+        gold: 3
+    },
+    {
+        name: 'Đạp xe',
+        gold: 4
+    },
+    {
+        name: 'Đấu kiếm',
+        gold: 5
+    },
+]
+function getTotalGold(array){
+    let total = 0;
+    for(var sport of array){
+        total += sport.gold;
+    }
+    return total;
+}
+
+// Expected results:
+console.log(getTotalGold(sports)) // Output: 23
