08c5af715b7f59c53f3935bd7c9c7f56b595d380
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-21 11:11:11 +0700

    Fix

--- JavaScriptBasic/13_HTML_DOM/Text/test.js
+++ JavaScriptBasic/13_HTML_DOM/Text/test.js
@@ -1,22 +1,18 @@
-//Nhiệm vụ 1: Thao tác với thẻ <a> 
-const myLink = document.getElementById("myLink");
+//1, tóm lấy thẻ div có id "container"
+const container = document.getElementById("container");
 
-//1. Đổi dòng chữ hiển thị.
-myLink.innerText = "Chuyển đến F8";
+//2. tạo ra 1 thẻ <button> mới (đang lơ lửng)
+const newButton = document.createElement("button");
 
-//2.Dùng setAttribute để đổi link(href)
-myLink.setAttribute("href", "https://f8.edu.vn" );
+//3. cài đặt nội dung chữ cho nút.
+newButton.innerText = "Click here!";
 
-//Nhiệm vụ 2: Thao tác với thẻ <div>
-const cardDiv = document.getElementById("card");
+//4. Gắn nút mới vào bên trong thẻ container (nó sẽ tự động nằm ở cuối)
+container.appendChild(newButton);
 
-//1. Thay đổi toàn bộ nội dung HTML bên trong 
-cardDiv.innerHTML = "<h2> Tiêu đề mới</h2> <p>Đã thay đổi bằng innerHTML</p>";
+//5. Tóm thẻ <p> cũ và xoá đi
+const pOld = document.getElementById("p-old");
+container.removeChild(pOld);
 
-//2. Dùng style đổi màu nền và màu chữ(quy tắc CamelCase)
-cardDiv.style.backgroundColor = "lightblue";
-cardDiv.style.color = "red";
 
-//3. Dùng classList.add() để thêm class 'shadow'
-cardDiv.classList.add("shadow");
 
