16d1cf4afd823bb8cd1255a381038f5dba5703da
Nhat-Huy-05 <bui85847@gmail.com>
2026-04-27 11:46:23 +0700

    bổ sung grid

--- tailwind-learning/app/page.tsx
+++ tailwind-learning/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="m-4 p-4 w-1/2 bg-yellow-200">
-      <p>Đây là chỗ viết content</p>
+    <div className="flex justify-start, items-center, bg-gray-800, text-white ">
+      <p>Logo</p>
+      <p>About</p>
+      <p>Contact</p>
     </div>
   )
 }
